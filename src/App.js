e0bceea5bcef984f61696da6a84e84ff1ca8e526
indhu-github <indhusworkmail@gmail.com>
2021-06-25 17:55:36 +0530

    development

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import "./App.css";
 import Home from "./pages/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <h1>Hello world welcome to our page</h1>
-      <Home />
+      <Router>
+        <Switch>
+          <Route path="/search">
+            <h1>Hello world welcome to our page</h1>
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
