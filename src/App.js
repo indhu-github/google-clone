c7abc60060e876fc5adff7a984a45ca0e15b48fd
indhu-github <indhusworkmail@gmail.com>
2021-06-25 17:41:05 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Hello world welcome to our page</h1>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>Home page</h1>
+      <div className="home_header">
+        <div class="home_headerLeft"></div>
+        <div class="home_headerRight"></div>
+      </div>
+      <div className="home_body"></div>
+    </div>
+  );
+}
+
+export default Home;
