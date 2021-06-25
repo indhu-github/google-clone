067e00252dbaf6f2e771432aab07cd24b62cfb81
indhu-github <indhusworkmail@gmail.com>
2021-06-25 19:34:31 +0530

    home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,20 @@
 import React from "react";
 import "./Home.css";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
     <div className="home">
       <h1>Home page</h1>
       <div className="home_header">
-        <div class="home_headerLeft"></div>
-        <div class="home_headerRight"></div>
+        <div class="home_headerLeft">
+          <Link to="/about">About</Link>
+          <Link to="/about">Store</Link>
+        </div>
+        <div class="home_headerRight">
+          <Link to="/about">Gmail</Link>
+          <Link to="/about">Images</Link>
+        </div>
       </div>
       <div className="home_body"></div>
     </div>
