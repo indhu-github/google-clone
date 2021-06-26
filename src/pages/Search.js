4265549d244b7b8b4b9276643fa5d049d9e71a5e
indhu-github <indhusworkmail@gmail.com>
2021-06-26 16:33:24 +0530

    search bar styling

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchIcon from "@material-ui/icons/Search";
 import MicIcon from "@material-ui/icons/Mic";
 import "./Search.css";
 import { Button } from "@material-ui/core";
 
 const Search = () => {
+  const [input, setInput] = useState("");
+
+  const search = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="search">
       <div class="search_input">
@@ -13,7 +19,9 @@ const Search = () => {
         <MicIcon />
       </div>
       <div class="search_buttons">
-        <Button variant="outlined">Google search</Button>
+        <Button onClick={search} variant="outlined">
+          Google search
+        </Button>
         <Button variant="outlined">I'm feeling lucky</Button>
       </div>
     </div>
