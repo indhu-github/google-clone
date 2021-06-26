3609b8dc28553ca3678f3391730f49953805cd54
indhu-github <indhusworkmail@gmail.com>
2021-06-26 15:21:47 +0530

    search bar styling

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,6 +1,8 @@
 import React from "react";
 import SearchIcon from "@material-ui/icons/Search";
 import MicIcon from "@material-ui/icons/Mic";
+import "./Search.css";
+
 const Search = () => {
   return (
     <div className="search">
