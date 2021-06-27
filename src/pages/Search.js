5d96bb85a9674e870cd64e78fe2c10cf7a464aa0
indhu-github <indhusworkmail@gmail.com>
2021-06-27 17:31:43 +0530

    input handling :heart:

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -4,13 +4,21 @@ import MicIcon from "@material-ui/icons/Mic";
 import "./Search.css";
 import { Button } from "@material-ui/core";
 import { useHistory } from "react-router-dom";
+import { useStateValue } from "../StateProvider";
+import { actionTypes } from "../reducer";
 
 const Search = ({ hideButtons = false }) => {
+  const [{}, dispatch] = useStateValue();
+
   const [input, setInput] = useState("");
   const history = useHistory();
   const search = (e) => {
     e.preventDefault();
     console.log("input handling");
+    dispatch({
+      type: actionTypes.SET_SEARCH_TERM,
+      term: input,
+    });
     history.push("/search");
   };
 
---
+++ src/pages/SearchPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./SearchPage.css";
+
+const SearchPage = () => {
+  return <div className="searchPage"></div>;
+};
+
+export default SearchPage;
