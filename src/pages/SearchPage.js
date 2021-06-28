ab41867bc76cc27a2177ba4b85a095b4b82f6330
indhu-github <indhusworkmail@gmail.com>
2021-06-28 20:44:13 +0530

    searchpage :hammer:

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,8 +1,18 @@
 import React from "react";
+import { useStateValue } from "../StateProvider";
 import "./SearchPage.css";
 
 const SearchPage = () => {
-  return <div className="searchPage"></div>;
+  const [{ term }, dispatch] = useStateValue();
+  return (
+    <div className="searchPage">
+      <div class="searchPage_header">
+        <h1>{term}</h1>
+      </div>
+
+      <div class="searchPage_results"></div>
+    </div>
+  );
 };
 
 export default SearchPage;
