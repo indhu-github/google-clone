dbf2aed80f651c1d60c6891d995183bf00eec88c
indhu-github <indhusworkmail@gmail.com>
2021-06-28 21:18:38 +0530

    searchpage :hammer:

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { useStateValue } from "../StateProvider";
+import useGoogleSearch from "../useGoogleSearch";
 import "./SearchPage.css";
 
 const SearchPage = () => {
   const [{ term }, dispatch] = useStateValue();
+  const { data } = useGoogleSearch(term);
 
-  
   return (
     <div className="searchPage">
       <div className="searchPage_header">
--- src/useGoogleSearch.js
+++ src/useGoogleSearch.js
@@ -3,16 +3,23 @@ import API_KEY from "./keys";
 
 const CONTEXT_KEY = "1175ea6c4bb3c0fa0";
 
-const useGoogleSearch = () => {
+//It is a custom hook
+const useGoogleSearch = ({ term }) => {
   const [data, setData] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
       fetch(
         `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${term}` //the endpoint that connects to the google custom serach api
-      ).then((response) => response.json());
+      )
+        .then((response) => response.json())
+        .then((result) => {
+          setData(result);
+        });
     };
+    fetchData();
   }, [term]);
+  return { data };
 };
 
 export default useGoogleSearch;
