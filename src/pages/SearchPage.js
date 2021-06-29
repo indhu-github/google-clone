50682fd3533db73bca4a1c8846c5b7982e0a5dc9
indhu-github <indhusworkmail@gmail.com>
2021-06-29 15:19:24 +0530

    searchPage options for result :hammer:

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -5,6 +5,12 @@ import "./SearchPage.css";
 import Response from "../response";
 import { Link } from "@material-ui/core";
 import Search from "./Search";
+import SearchIcon from "@material-ui/icons/Search";
+import DescriptionIcon from "@material-ui/icons/Description";
+import ImageIcon from "@material-ui/icons/Image";
+import LocalOfferIcon from "@material-ui/icons/LocalOffer";
+import RoomIcon from "@material-ui/icons/Room";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
 
 const SearchPage = () => {
   const [{ term }, dispatch] = useStateValue();
@@ -25,7 +31,35 @@ const SearchPage = () => {
           />
         </Link>
         <div className="searchPage_headerBody">
-          <Search hidebuttons />
+          <Search hideButtons />
+          <div class="searchPage_options">
+            <div class="searchPage_optionsLeft">
+              <div className="searchPage_option">
+                <SearchIcon />
+                <Link to="/all">All</Link>
+              </div>
+              <div className="searchPage_option">
+                <DescriptionIcon />
+                <Link to="/all">News</Link>
+              </div>
+              <div className="searchPage_option">
+                <ImageIcon />
+                <Link to="/all">Images</Link>
+              </div>
+              <div className="searchPage_option">
+                <LocalOfferIcon />
+                <Link to="/all">Shopping</Link>
+              </div>
+              <div className="searchPage_option">
+                <RoomIcon />
+                <Link to="/all">Maps</Link>
+              </div>
+              <div className="searchPage_option">
+                <MoreVertIcon />
+                <Link to="/all">More</Link>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
 
